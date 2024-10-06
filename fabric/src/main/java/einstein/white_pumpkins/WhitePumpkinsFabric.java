58ff51b001443233957199350062a8392b63c11d
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-10-06 17:00:40 -0400

    Added stuff to creative tabs

--- fabric/src/main/java/einstein/white_pumpkins/WhitePumpkinsFabric.java
+++ fabric/src/main/java/einstein/white_pumpkins/WhitePumpkinsFabric.java
@@ -4,8 +4,13 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
 
 public class WhitePumpkinsFabric implements ModInitializer, ClientModInitializer {
 
@@ -17,6 +22,15 @@ public class WhitePumpkinsFabric implements ModInitializer, ClientModInitializer
         CompostingChanceRegistry.INSTANCE.add(ModInit.CARVED_WHITE_PUMPKIN.get(), 0.65F);
         CompostingChanceRegistry.INSTANCE.add(ModInit.WHITE_PUMPKIN_SEEDS.get(), 0.3F);
         CompostingChanceRegistry.INSTANCE.add(ModInit.WHITE_PUMPKIN_PIE.get(), 1F);
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.NATURAL_BLOCKS).register(entries -> {
+            entries.addAfter(Blocks.PUMPKIN, new ItemStack(ModInit.WHITE_PUMPKIN.get()));
+            entries.addAfter(Blocks.CARVED_PUMPKIN, new ItemStack(ModInit.CARVED_WHITE_PUMPKIN.get()));
+            entries.addAfter(Blocks.JACK_O_LANTERN, new ItemStack(ModInit.WHITE_JACK_O_LANTERN.get()));
+            entries.addAfter(Items.PUMPKIN_SEEDS, new ItemStack(ModInit.WHITE_PUMPKIN_SEEDS.get()));
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(entries -> {
+            entries.addAfter(Items.PUMPKIN_PIE, new ItemStack(ModInit.WHITE_PUMPKIN_PIE.get()));
+        });
     }
 
     @Override
--- neoforge/src/main/java/einstein/white_pumpkins/WhitePumpkinsNeoForge.java
+++ neoforge/src/main/java/einstein/white_pumpkins/WhitePumpkinsNeoForge.java
@@ -1,11 +1,17 @@
 package einstein.white_pumpkins;
 
 import einstein.white_pumpkins.platform.NeoForgeRegistryHelper;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
-import net.neoforged.neoforge.client.event.RegisterNamedRenderTypesEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
 @Mod(WhitePumpkins.MOD_ID)
 public class WhitePumpkinsNeoForge {
@@ -20,5 +26,17 @@ public class WhitePumpkinsNeoForge {
                         WhitePumpkins.registerBlockColors(state, pos, tintGetter, tintIndex)
                 )
         );
+        modEventBus.addListener((BuildCreativeModeTabContentsEvent event) -> {
+            ResourceKey<CreativeModeTab> tabKey = event.getTabKey();
+            if (tabKey.equals(CreativeModeTabs.NATURAL_BLOCKS)) {
+                event.insertAfter(new ItemStack(Blocks.PUMPKIN), new ItemStack(ModInit.WHITE_PUMPKIN.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                event.insertAfter(new ItemStack(Blocks.CARVED_PUMPKIN), new ItemStack(ModInit.CARVED_WHITE_PUMPKIN.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                event.insertAfter(new ItemStack(Blocks.JACK_O_LANTERN), new ItemStack(ModInit.WHITE_JACK_O_LANTERN.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                event.insertAfter(new ItemStack(Items.PUMPKIN_SEEDS), new ItemStack(ModInit.WHITE_PUMPKIN_SEEDS.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            }
+            else if (tabKey.equals(CreativeModeTabs.FOOD_AND_DRINKS)) {
+                event.insertAfter(new ItemStack(Items.PUMPKIN_PIE), new ItemStack(ModInit.WHITE_PUMPKIN_PIE.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            }
+        });
     }
 }
