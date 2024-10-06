da0d02f034195eac247ff83e9b445cf0afab25c3
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-10-06 00:53:55 -0400

    Composting

--- fabric/src/main/java/einstein/white_pumpkins/WhitePumpkinsFabric.java
+++ fabric/src/main/java/einstein/white_pumpkins/WhitePumpkinsFabric.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.minecraft.client.renderer.RenderType;
 
 public class WhitePumpkinsFabric implements ModInitializer, ClientModInitializer {
@@ -11,6 +12,11 @@ public class WhitePumpkinsFabric implements ModInitializer, ClientModInitializer
     @Override
     public void onInitialize() {
         WhitePumpkins.init();
+
+        CompostingChanceRegistry.INSTANCE.add(ModInit.WHITE_PUMPKIN.get(), 0.65F);
+        CompostingChanceRegistry.INSTANCE.add(ModInit.CARVED_WHITE_PUMPKIN.get(), 0.65F);
+        CompostingChanceRegistry.INSTANCE.add(ModInit.WHITE_PUMPKIN_SEEDS.get(), 0.3F);
+        CompostingChanceRegistry.INSTANCE.add(ModInit.WHITE_PUMPKIN_PIE.get(), 1F);
     }
 
     @Override
