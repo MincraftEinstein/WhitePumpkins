1a6ce7d24ae2f25682a45a73c05ad48e9af3aa66
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-11-15 17:33:07 -0500

    Changed white pumpkin stem colors

--- common/src/main/java/einstein/white_pumpkins/mixin/BlockColorsMixin.java
+++ common/src/main/java/einstein/white_pumpkins/mixin/BlockColorsMixin.java
@@ -1,43 +1,27 @@
 package einstein.white_pumpkins.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import einstein.white_pumpkins.ModInit;
-import net.minecraft.client.color.block.BlockColor;
+import einstein.white_pumpkins.WhitePumpkins;
+import einstein.white_pumpkins.block.WhitePumpkinStemBlock;
 import net.minecraft.client.color.block.BlockColors;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.util.ARGB;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockColors.class)
 public class BlockColorsMixin {
 
-    @WrapOperation(method = "createDefault", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/color/block/BlockColors;addColoringState(Lnet/minecraft/world/level/block/state/properties/Property;[Lnet/minecraft/world/level/block/Block;)V"))
-    private static void create(BlockColors instance, Property<?> property, Block[] blocks, Operation<Void> original) {
-        original.call(instance, property, whitePumpkins$addWhitePumpkinStem(blocks));
-    }
-
-    @WrapOperation(method = "createDefault", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/color/block/BlockColors;register(Lnet/minecraft/client/color/block/BlockColor;[Lnet/minecraft/world/level/block/Block;)V"))
-    private static void create(BlockColors instance, BlockColor blockColor, Block[] blocks, Operation<Void> original) {
-        original.call(instance, blockColor, whitePumpkins$addWhitePumpkinStem(blocks));
-    }
-
-    @Unique
-    private static Block[] whitePumpkins$addWhitePumpkinStem(Block[] blocks) {
-        List<Block> list = new ArrayList<>(List.of(blocks));
-        if (list.contains(Blocks.PUMPKIN_STEM)) {
-            list.add(ModInit.WHITE_PUMPKIN_STEM.get());
-        }
-
-        if (list.contains(Blocks.ATTACHED_PUMPKIN_STEM)) {
-            list.add(ModInit.ATTACHED_WHITE_PUMPKIN_STEM.get());
-        }
-        return list.toArray(Block[]::new);
+    @Inject(method = "createDefault", at = @At("TAIL"))
+    private static void create(CallbackInfoReturnable<BlockColors> cir, @Local BlockColors blockColors) {
+        blockColors.register((state, tintGetter, pos, tintIndex) -> {
+            int age = state.getValue(WhitePumpkinStemBlock.AGE);
+            int i = age + 1;
+            int i1 = (int) (((255F - 32F) / WhitePumpkinStemBlock.MAX_AGE) * age);
+            return ARGB.color(32 + i1, 247 + i, 4 * i + i1);
+        }, ModInit.WHITE_PUMPKIN_STEM.get());
+        ((BlockColorsAccessor) blockColors).whitePumpkins$addColoringState(WhitePumpkinStemBlock.AGE, ModInit.WHITE_PUMPKIN_STEM.get());
     }
 }
