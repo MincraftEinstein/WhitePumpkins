967b0888071d373754110fd2c4285d2590e21490
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2025-06-10 19:43:35 -0400

    Waring a carved white pumpkin will now hide your name tag

---
+++ common/src/main/java/einstein/white_pumpkins/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,23 @@
+package einstein.white_pumpkins.mixin;
+
+import einstein.white_pumpkins.ModInit;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntityRenderer.class)
+public class LivingEntityRendererMixin<T extends LivingEntity> {
+
+    @Inject(method = "shouldShowName*", at = @At("HEAD"), cancellable = true)
+    private void shouldShowName(T entity, double d, CallbackInfoReturnable<Boolean> cir) {
+        if (entity.getItemBySlot(EquipmentSlot.HEAD).is(ModInit.CARVED_WHITE_PUMPKIN.get().asItem())) {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+    }
+}
